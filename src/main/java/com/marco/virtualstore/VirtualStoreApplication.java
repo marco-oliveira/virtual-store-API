50ca35b1cfd8842b1a5a50a3be20ab4304363bf3
marco-oliveira <marco-oliveira@live.com>
2018-05-18 13:38:22 -0300

    Instanciando e salvando no Banco Categorias, no boot da aplicação

--- src/main/java/com/marco/virtualstore/VirtualStoreApplication.java
+++ src/main/java/com/marco/virtualstore/VirtualStoreApplication.java
@@ -1,12 +1,28 @@
 package com.marco.virtualstore;
 
+import com.marco.virtualstore.domains.Categoria;
+import com.marco.virtualstore.repositories.CategoriaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Arrays;
+
 @SpringBootApplication
-public class VirtualStoreApplication {
+public class VirtualStoreApplication implements CommandLineRunner {
+
+    @Autowired
+    private CategoriaRepository categoriaRepository;
 
     public static void main(String[] args) {
         SpringApplication.run(VirtualStoreApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        this.categoriaRepository.saveAll(Arrays.asList(new Categoria(null, "Informática"),
+                                                        new Categoria(null, "Escritório")));
+
     }
 }
