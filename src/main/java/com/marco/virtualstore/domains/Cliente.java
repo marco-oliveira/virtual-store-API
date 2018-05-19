04f702743fe1574f9d188e54a66738d4d9e442a7
marco-oliveira <marco-oliveira@live.com>
2018-05-19 09:40:47 -0300

    Criação e Relacionamentos entre as entidades Cliente, Endereço, TipoCliente e Telefone

---
+++ src/main/java/com/marco/virtualstore/domains/Cliente.java
@@ -0,0 +1,118 @@
+package com.marco.virtualstore.domains;
+
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import com.marco.virtualstore.domains.enums.TipoCliente;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+/**
+ * Created by Marco Antônio on 19/05/2018
+ */
+@Entity
+public class Cliente implements Serializable {
+
+    private static final long serialVersionUID = -5358043810526203148L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+
+    private String email;
+
+    private String cpfOuCnpj;
+
+    private Integer tipo;
+
+    @JsonManagedReference
+    @OneToMany(mappedBy = "cliente")
+    private List<Endereco> enderecos = new ArrayList<>();
+
+    @ElementCollection
+    private List<String> telefones = new ArrayList<>();
+
+    public Cliente(){}
+
+    public Cliente(Long id, String nome, String email, String cpfOuCnpj, TipoCliente tipo) {
+        this.id = id;
+        this.nome = nome;
+        this.email = email;
+        this.cpfOuCnpj = cpfOuCnpj;
+        this.tipo = tipo.getCod();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getCpfOuCnpj() {
+        return cpfOuCnpj;
+    }
+
+    public void setCpfOuCnpj(String cpfOuCnpj) {
+        this.cpfOuCnpj = cpfOuCnpj;
+    }
+
+    public TipoCliente getTipo() {
+        return TipoCliente.toEnum(tipo);
+    }
+
+    public void setTipo(TipoCliente tipo) {
+        this.tipo = tipo.getCod();
+    }
+
+    public List<Endereco> getEnderecos() {
+        return enderecos;
+    }
+
+    public void setEnderecos(List<Endereco> enderecos) {
+        this.enderecos = enderecos;
+    }
+
+    public List<String> getTelefones() {
+        return telefones;
+    }
+
+    public void setTelefones(List<String> telefones) {
+        this.telefones = telefones;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Cliente cliente = (Cliente) o;
+        return Objects.equals(id, cliente.id);
+    }
+
+    @Override
+    public int hashCode() {
+
+        return Objects.hash(id);
+    }
+}
---
+++ src/main/java/com/marco/virtualstore/domains/enums/TipoCliente.java
@@ -0,0 +1,39 @@
+package com.marco.virtualstore.domains.enums;
+
+/**
+ * Created by Marco Antônio on 19/05/2018
+ */
+public enum TipoCliente {
+
+    PESSOAFISICA(1, "Pessoa Física"),
+    PESSOAJURIDICA(2, "Pessoa Jurídica");
+
+    private int cod;
+    private String descricao;
+
+    TipoCliente(Integer cod, String descricao) {
+        this.cod = cod;
+        this.descricao = descricao;
+    }
+
+    public int getCod() {
+        return cod;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public static TipoCliente toEnum(Integer cod){
+        if (cod == null){
+            return null;
+        }
+
+        for (TipoCliente tipo: TipoCliente.values()) {
+            if (cod.equals(tipo.getCod())){
+                return tipo;
+            }
+        }
+        throw new IllegalArgumentException("Código Inválido: "+cod);
+    }
+}
