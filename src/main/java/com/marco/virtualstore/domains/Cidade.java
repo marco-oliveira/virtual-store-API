d567f72abb6e66735d6632eb8a001361899420dc
marco-oliveira <marco-oliveira@live.com>
2018-05-19 09:01:49 -0300

    Entidades Estado e Cidade, e seus relacionamentos

---
+++ src/main/java/com/marco/virtualstore/domains/Cidade.java
@@ -0,0 +1,75 @@
+package com.marco.virtualstore.domains;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Objects;
+
+/**
+ * Created by Marco Antônio on 19/05/2018
+ */
+
+@Entity
+public class Cidade implements Serializable {
+
+    private static final long serialVersionUID = -595432622413136935L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+
+    @JsonBackReference
+    @ManyToOne
+    @JoinColumn(name = "estado_id")
+    private Estado estado;
+
+    public Cidade() {
+    }
+
+    public Cidade(Long id, String nome) {
+        this.id = id;
+        this.nome = nome;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public Estado getEstado() {
+        return estado;
+    }
+
+    public void setEstado(Estado estado) {
+        this.estado = estado;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Cidade cidade = (Cidade) o;
+        return Objects.equals(id, cidade.id);
+    }
+
+    @Override
+    public int hashCode() {
+
+        return Objects.hash(id);
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+}
