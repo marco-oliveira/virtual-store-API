ade53eeebadf07f7451c034e756a4ff094d159a8
marco-oliveira <marco-oliveira@live.com>
2018-06-05 11:26:08 -0300

    Validação customizada de Email Existente

--- src/main/java/com/marco/virtualstore/repositories/ClienteRepository.java
+++ src/main/java/com/marco/virtualstore/repositories/ClienteRepository.java
@@ -3,10 +3,14 @@ package com.marco.virtualstore.repositories;
 import com.marco.virtualstore.domains.Cliente;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
  * Created by Marco Antônio on 19/05/2018
  */
 @Repository
 public interface ClienteRepository extends JpaRepository<Cliente, Long> {
+
+    @Transactional(readOnly = true)
+    Cliente findByEmail(String email);
 }
--- src/main/java/com/marco/virtualstore/services/validations/ClienteInsertValidator.java
+++ src/main/java/com/marco/virtualstore/services/validations/ClienteInsertValidator.java
@@ -1,9 +1,12 @@
 package com.marco.virtualstore.services.validations;
 
+import com.marco.virtualstore.domains.Cliente;
 import com.marco.virtualstore.domains.enums.TipoCliente;
 import com.marco.virtualstore.dtos.NewClienteDto;
+import com.marco.virtualstore.repositories.ClienteRepository;
 import com.marco.virtualstore.resources.exception.FieldMessage;
 import com.marco.virtualstore.services.validations.utils.BR;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
@@ -11,6 +14,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, NewClienteDto> {
+
+    @Autowired
+    private ClienteRepository clienteRepository;
+
     @Override
     public void initialize(ClienteInsert constraintAnnotation) {
 
@@ -25,6 +32,11 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
                 !BR.isValidCPF(newClienteDto.getCpfOuCnpj())) list.add(new FieldMessage("cpfOuCnpj", "CPF inválido."));
         if (newClienteDto.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCod()) &&
                 !BR.isValidCNPJ(newClienteDto.getCpfOuCnpj())) list.add(new FieldMessage("cpfOuCnpj", "CNPJ inválido."));
+
+        Cliente byEmail = this.clienteRepository.findByEmail(newClienteDto.getEmail());
+
+        if (byEmail!= null)
+                list.add(new FieldMessage("email", "Email já existente."));
 
         for (FieldMessage e: list) {
             context.disableDefaultConstraintViolation();
