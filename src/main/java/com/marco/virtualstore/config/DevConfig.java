9495e128478ce25f96a709985f695b38fab343ae
marco-oliveira <marco-oliveira@live.com>
2018-06-06 20:15:13 -0300

    Criando profile de desenvolvedor, e usando mysql

---
+++ src/main/java/com/marco/virtualstore/config/DevConfig.java
@@ -0,0 +1,34 @@
+package com.marco.virtualstore.config;
+
+import com.marco.virtualstore.services.DBService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import java.text.ParseException;
+
+/**
+ * Created by Marco Antônio on 06/06/2018
+ */
+
+@Configuration
+@Profile("dev")
+public class DevConfig {
+
+    @Autowired
+    private DBService dbService;
+
+    @Value("${spring.jpa.hibernate.ddl-auto}")
+    private String strategy;
+
+    @Bean
+    public boolean instantiateDataBase() throws ParseException {
+
+        if (!strategy.equals("create")) return false;
+
+        this.dbService.instantiateTestDataBase();
+        return true;
+    }
+}
